cbc647d1963b262a8eef97a0c63f8eace5b64b3f
sritiz <sritizsahu07@gmail.com>
2026-07-05 17:18:22 +0400

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from './components/Layout.jsx';
 
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
+import Landing from './pages/Landing.jsx';
 import ScreeningDetail from './pages/ScreeningDetail.jsx';
 
 import ParentDashboard from './pages/parent/Dashboard.jsx';
@@ -36,11 +37,12 @@ export default function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
       <Route
-        path="/"
+        path="/dashboard"
         element={
           <ProtectedRoute>
             <Layout>
